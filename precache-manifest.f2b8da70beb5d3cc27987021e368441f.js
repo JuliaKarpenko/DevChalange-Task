401f94d15c608d8e8805038139a2f136628ac584
Admin <jule4ka.jd@gmail.com>
2019-05-19 11:13:26 +0300

    Updates

---
+++ precache-manifest.f2b8da70beb5d3cc27987021e368441f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5ca503ae570e467efed4f2dcfc1cf93",
+    "url": "/DevChalange-Task/static/media/fontello.f5ca503a.ttf"
+  },
+  {
+    "revision": "cdadc4e4f43fd03e40491dd80bac0ea2",
+    "url": "/DevChalange-Task/static/media/fontello.cdadc4e4.woff"
+  },
+  {
+    "revision": "6303130d8cbdc993eaecacb682431219",
+    "url": "/DevChalange-Task/static/media/fontello.6303130d.woff2"
+  },
+  {
+    "revision": "4a25d8128e45b4c154660ebc95fdf630",
+    "url": "/DevChalange-Task/static/media/fontello.4a25d812.eot"
+  },
+  {
+    "revision": "07da5955e1f6567a81d61ffd6d938d2c",
+    "url": "/DevChalange-Task/static/media/fontello.07da5955.svg"
+  },
+  {
+    "revision": "885249846710600a20db",
+    "url": "/DevChalange-Task/static/js/runtime~main.9f1beb25.js"
+  },
+  {
+    "revision": "b31c466c8cbca67126c0",
+    "url": "/DevChalange-Task/static/js/main.a1184af2.chunk.js"
+  },
+  {
+    "revision": "b083f632a90e09a2956f",
+    "url": "/DevChalange-Task/static/js/2.956ec289.chunk.js"
+  },
+  {
+    "revision": "b31c466c8cbca67126c0",
+    "url": "/DevChalange-Task/static/css/main.3d98c0c1.chunk.css"
+  },
+  {
+    "revision": "5be92243b7d848500d3cf6a05af35f11",
+    "url": "/DevChalange-Task/index.html"
+  }
+];
